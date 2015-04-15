a962487f58986c65705336f845db38f7b7ab389d
sajithks <isajith@gmail.com>
2015-04-15 08:32:55 +0200

    all files modified

--- dataset_create.py
+++ dataset_create.py
@@ -9,39 +9,44 @@ Created on Wed Mar 11 16:35:28 2015
 
 import scipy as sp
 import time
-import sys
-import os
-sys.path.append('/Users/sajithks/Documents/project_cell_tracking/phase images from elf')
-sys.path.append('/Users/sajithks/Documents/project_cell_tracking/phase images from elf/ecoli_det_seg')
-import createbacteria  as cb
+#import sys
+#import os
+#sys.path.append('/Users/sajithks/Documents/project_cell_tracking/phase images from elf')
+#sys.path.append('/Users/sajithks/Documents/project_cell_tracking/phase images from elf/ecoli_det_seg')
+#import createbacteria  as cb
 import numpy as np
-from matplotlib import pyplot as plt
-import skimage.morphology as skmorph
+#from matplotlib import pyplot as plt
+#import skimage.morphology as skmorph
 import cv2
-from scipy.ndimage import label
-import fiteli
-from multiprocessing import Pool
+#from scipy.ndimage import label
+#import fiteli
+#from multiprocessing import Pool
 #from multiprocessing.pool import ThreadPool as Pool
 #from numba import jit, double
 #import cmath
-from skimage import transform
-import scipy.ndimage
-import pymeanshift as pms
-import morphsnakes
-import criticalpoints as cr
-import skimage
-from skimage.morphology import watershed
+#from skimage import transform
+#import scipy.ndimage
+#import pymeanshift as pms
+#import morphsnakes
+#import criticalpoints as cr
+#import skimage
+#from skimage.morphology import watershed
 #import random
-from random import randrange
-
-#%%
-orimg = cv2.imread('/Users/sajithks/Documents/project_cell_tracking/processed/result_comparisons/tracking/phaseimage/img/ex_Phase0001.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
-segimage = cv2.imread('/Users/sajithks/Documents/project_cell_tracking/processed/result_comparisons/tracking/phaseimage/Analysis/Segmentation/img/labeled20141021_ex_Phase0001.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
-fgcoord = np.argwhere(segimage>0)
-bgcoord = np.argwhere(segimage==0)
+#from random import randrange
 
 WIN_SIZE = 10
 WINDOW = 2*WIN_SIZE + 1
+
+#%% training set
+orimg = cv2.imread('/home/saj/Documents/deeptraing-master/data/img/ex_Phase0001.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+orimg = np.float32(orimg)
+orimg = orimg-orimg.min()
+orimg = np.uint8(255*(orimg/orimg.max()))
+
+segimage = cv2.imread('/home/saj/Documents/deeptraing-master/data/seg/labeled20141021_ex_Phase0001.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+fgcoord = np.argwhere(segimage>0)
+bgcoord = np.argwhere(segimage==0)
+
 
 #%%
 fgc = []
@@ -57,13 +62,14 @@ for ii in bgcoord:
         
 # training data
 
-outfolder = '/Users/sajithks/Documents/caffe_traindata/ecoli/traindata/'
+outfolder = '/home/saj/Downloads/caffe-master/examples/ecoli/traindata/'
 target = open(outfolder +'training', 'w')        
 for ii in np.unique(np.int32(np.linspace(0, min(np.shape(fgc)[0], np.shape(bgc)[0])-1, 50000 ))):
     savname = 'fg_' + np.str(fgc[ii][0]) +'_' + np.str(fgc[ii][1]) + '.png'
     savimg = orimg[fgc[ii][0] - WIN_SIZE:fgc[ii][0] + WIN_SIZE, fgc[ii][1] - WIN_SIZE:fgc[ii][1] + WIN_SIZE]
     cv2.imwrite( outfolder + savname, savimg)
-    target.write(savname) 
+    target.write(outfolder + savname)
+#    target.write(savname) 
     target.write(" ")
     target.write("1")
     target.write("\n")
@@ -72,25 +78,49 @@ for ii in np.unique(np.int32(np.linspace(0, min(np.shape(fgc)[0], np.shape(bgc)[
     savname = 'bg_' + np.str(bgc[ii][0]) +'_' + np.str(bgc[ii][1]) + '.png'
     savimg = orimg[(bgc[ii][0] - WIN_SIZE):(bgc[ii][0] + WIN_SIZE), (bgc[ii][1] - WIN_SIZE):(bgc[ii][1] + WIN_SIZE)]
     cv2.imwrite( outfolder + savname, savimg)
-    target.write(savname) 
+    target.write(outfolder + savname)
+#    target.write(savname) 
     target.write(" ") 
     target.write("0")
     target.write("\n")
 #    countval = countval + 1
 
 target.close()
+#%% #################### testing set ########################################
+orimg = cv2.imread('/home/saj/Documents/deeptraing-master/data/img/ex_Phase0010.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+orimg = np.float32(orimg)
+orimg = orimg-orimg.min()
+orimg = np.uint8(255*(orimg/orimg.max()))
 
-#%%
+segimage = cv2.imread('/home/saj/Documents/deeptraing-master/data/seg/labeled20141021_ex_Phase0010.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+#orimg = cv2.resize(orimg,(430,430))
+#segimage = cv2.resize(segimage, (430,430))
+
+fgcoord = np.argwhere(segimage>0)
+bgcoord = np.argwhere(segimage==0)
+
+fgc = []
+for ii in fgcoord:
+    if(ii[0]>WINDOW and ii[1]>WINDOW and ii[0]<orimg.shape[0]-WINDOW and ii[1]<orimg.shape[1]-WINDOW):
+        fgc.append(ii)
+        
+bgc = []
+for ii in bgcoord:
+    if(ii[0]>WINDOW and ii[1]>WINDOW and ii[0]<orimg.shape[0]-WINDOW and ii[1]<orimg.shape[1]-WINDOW):
+        bgc.append(ii)
+
         
 # testing data
-outfolder = '/Users/sajithks/Documents/caffe_traindata/ecoli/testdata/'
+
+outfolder = '/home/saj/Downloads/caffe-master/examples/ecoli/testdata/'
 target = open(outfolder +'testing', 'w')        
 
-for ii in np.unique(np.int32(np.linspace(0, min(np.shape(fgc)[0], np.shape(bgc)[0])-1, 1024 ))):
+for ii in np.unique(np.int32(np.linspace(0, min(np.shape(fgc)[0], np.shape(bgc)[0])-1, 10000 ))):
     savname = 'fg_' + np.str(fgc[ii][0]) +'_' + np.str(fgc[ii][1]) + '.png'
     savimg = orimg[fgc[ii][0] - WIN_SIZE:fgc[ii][0] + WIN_SIZE, fgc[ii][1] - WIN_SIZE:fgc[ii][1] + WIN_SIZE]
     cv2.imwrite( outfolder + savname, savimg)
-    target.write(savname) 
+#    target.write(savname) 
+    target.write(outfolder + savname)
     target.write(" ")
     target.write("1")
     target.write("\n")
@@ -99,7 +129,8 @@ for ii in np.unique(np.int32(np.linspace(0, min(np.shape(fgc)[0], np.shape(bgc)[
     savname = 'bg_' + np.str(bgc[ii][0]) +'_' + np.str(bgc[ii][1]) + '.png'
     savimg = orimg[(bgc[ii][0] - WIN_SIZE):(bgc[ii][0] + WIN_SIZE), (bgc[ii][1] - WIN_SIZE):(bgc[ii][1] + WIN_SIZE)]
     cv2.imwrite( outfolder + savname, savimg)
-    target.write(savname) 
+#    target.write(savname) 
+    target.write(outfolder + savname)
     target.write(" ") 
     target.write("0")
     target.write("\n")
---
+++ netfeatureextract.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 08:26:53 2015
+
+@author: saj
+"""
+
+import scipy as sp
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+import Image
+
+# Make sure that caffe is on the python path:
+caffe_root = '/home/saj/Downloads/caffe-master/'  # this file is expected to be in {caffe_root}/examples
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+import time
+import sklearn
+import random
+from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.ensemble import AdaBoostClassifier as ab
+from multiprocessing import Pool
+
+print 'libraries loaded'
+#%%
+# Load the net, list its data and params, and filter an example image.
+caffe.set_mode_cpu()
+net = caffe.Net(caffe_root + 'examples/ecoli/ecolifile2deploy.prototxt',
+                caffe_root + 'examples/ecoli/file8bit2_iter_10000.caffemodel',
+                caffe.TEST)
+
+######################## training #######################################
+#%% read images
+print 'loading image and label ...'
+orimg = cv2.imread('/home/saj/Documents/deeptraing-master/data/img/ex_Phase0003.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+orimg = np.float32(orimg)
+orimg = orimg-orimg.min()
+orimg = np.uint8(255*(orimg/orimg.max()))
+
+segimg = cv2.imread('/home/saj/Documents/deeptraing-master/data/seg/labeled20141021_ex_Phase0003.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+labimg = (segimg>0)*1
+print('image and label loaded')
+#%%
+print 'extracting features ...'
+st = time.time()
+params = ['conv1', 'conv2', 'conv3']
+c = 0
+featmat = []
+for kk in params:
+    for ii in range(np.shape(net.params[kk][0].data)[0]):
+        for jj in range(np.shape(net.params['conv1'][0].data[ii])[0]):
+            filt = net.params['conv1'][0].data[ii][jj]     
+            featmat.append(sp.ndimage.convolve(orimg,filt))
+            
+print time.time()-st
+featmat = np.array(featmat)
+print 'features extracted'
+#%%
+print 'training ...'
+feat = []
+lab = []
+
+#for ii in range(10000):
+#    row = random.randint(0,orimg.shape[0]-1)
+#    col = random.randint(0,orimg.shape[1]-1)
+#    feat.append(featmat[:,row,col])
+#    lab.append(labimg[row,col])
+for row in np.int32(np.linspace(0, orimg.shape[0]-1, 315)):
+    for col in np.int32(np.linspace(0, orimg.shape[1]-1, 315)):
+        feat.append(featmat[:, row, col])
+        lab.append(labimg[row, col])
+    
+feat = np.array(feat)
+lab = np.array(lab)
+
+rforest = rf(n_estimators=200)
+
+rforest.fit(feat,lab)
+print 'training done.'
+#%%
+st = time.time()
+adaboost = ab(n_estimators=200)
+adaboost.fit(feat,lab)
+print time.time()-st
+#%%
+############################### testing ###############################
+print 'test data loading ...'
+st = time.time()
+inimg = cv2.imread('/home/saj/Documents/deeptraing-master/data/img/ex_Phase0004.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+inimg = np.float32(inimg)
+inimg = inimg-inimg.min()
+inimg = np.uint8(255*(inimg/inimg.max()))
+
+params = ['conv1', 'conv2', 'conv3']
+c = 0
+print 'test data loaded'
+
+print 'feature extracting...'
+
+featmat = []
+for kk in params:
+    for ii in range(np.shape(net.params[kk][0].data)[0]):
+        for jj in range(np.shape(net.params['conv1'][0].data[ii])[0]):
+            filt = net.params['conv1'][0].data[ii][jj]     
+            featmat.append(sp.ndimage.convolve(inimg,filt))
+            
+print time.time()-st
+featmat = np.array(featmat)
+print 'feature extraction done'
+#%%
+print 'start classification...'
+st = time.time()
+outimg = np.zeros((inimg.shape[0], inimg.shape[1]))
+#featreshape = featmat.reshape(featmat.shape[0],featmat.shape[1]*featmat.shape[2]).T
+#outimg = rforest.predict(featreshape).reshape(featmat.shape[1],featmat.shape[2])
+
+for ii in range(inimg.shape[1]):
+#    print ii
+#    outimg[ii,:] = rforest.predict(featmat[:,ii,:].T)    
+    outimg[ii,:] =rforest.predict_proba(featmat[:,ii,:].T)[:,0]
+
+  
+print 'testing time ', time.time() - st
+
+plt.imshow(outimg)
+print 'classification done!'
+
+
+
