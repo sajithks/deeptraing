ce14bb76d95d1b3e8cd97d71754a82d4f20cb07b
sajithks <sajithks@dhcp19.cb.uu.se>
2015-05-22 14:44:27 +0200

    edited segment evalutaion

--- segevaluation.py
+++ segevaluation.py
@@ -21,9 +21,11 @@ import scipy as sp
 from matplotlib import pyplot as plt
 from matplotlib.legend_handler import HandlerLine2D
 import cv2
+import glob
 #import time
 import scipy.io as sio
-
+import criticalpoints2 as cp
+import string
 from scipy.ndimage import label
 
 def fthreshValue(fmeasure,binval):
@@ -32,6 +34,27 @@ def fthreshValue(fmeasure,binval):
     for ii in range(a.shape[0]):
         fthresh[ii] = (fmeasure > a[ii] ).tolist().count(True)
     return(fthresh/fmeasure.shape[0])
+
+def labelgt(gt):
+    mask = gt==1
+    seed, ncc = label(gt==2,np.ones((3,3)))
+    gtlabel = cb.watershedSeeded(np.ones(gt.shape), mask, seed)
+    return(gtlabel)
+
+def labelSegimg(img):
+    segimg = np.argmax(img, 2)
+    segimglab, segncc = label( segimg==0, np.ones((3,3)) )
+    segseed = (segimg==2)*1
+    segseed = segseed*(sp.ndimage.binary_fill_holes(segimg==0)>0)
+    
+    segimglab = cb.removeBoundaryLabel(segimglab)        
+    segmask = (segimglab>1)*1    
+    segseed, ncc = label(segseed, np.ones((3,3)))
+    #segseed = cb.removeBoundaryLabel(segseed)
+    
+    segimglab = cb.watershedSeeded(np.ones(segmask.shape), segmask, segseed)
+    
+    return(segimglab)
 
 bincount = 100
 LINEWIDTH = 1
@@ -48,83 +71,169 @@ plt.rc('font', **font)
 
 gtfolder = '/Users/sajithks/Documents/dump/data_neutrophils/ilastik/'
 
-rffolder = '/Users/sajithks/Documents/deeptraing/data_neutrophils/output/randomforest/ver3/'
+#neural net
 neuralfolder = '/Users/sajithks/Documents/deeptraing/data_neutrophils/output/neuralnet_caffedirect/ver3/'
-#% gt
+neuraltrainfiles = sorted(glob.glob(neuralfolder + '*Aligned0000.tif'))
+neuraltestfiles = sorted(glob.glob(neuralfolder + '*Aligned0010.tif'))
+
+#random forest
+rffolder = '/Users/sajithks/Documents/deeptraing/data_neutrophils/output/randomforest/ver3/'
+rftrainfiles = sorted(glob.glob(rffolder + '*Aligned0000.tif'))
+rftestfiles = sorted(glob.glob(rffolder + '*Aligned0010.tif'))
+
+#% ground truth
 startime = time.time()
-gt = cv2.imread(gtfolder+'Labels00.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
-
-mask = gt==1
-seed, ncc = label(gt==2,np.ones((3,3)))
-gtlabel = cb.watershedSeeded(np.ones(gt.shape), mask, seed)
-#gtlabel[gtlabel==-1]=0
-#gtlabel[gtlabel==1]=0
+gttrain = cv2.imread(gtfolder+'Labels00.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
+gttest = cv2.imread(gtfolder+'Labels10.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
 
 
+gttrainlab = labelgt(gttrain)
+gttestlab = labelgt(gttest)
 
-
-rfseg = cv2.imread(rffolder+'neutro_conv_1_2_4_Aligned0000.tif',cv2.CV_LOAD_IMAGE_UNCHANGED)
-rfseg = rfseg[6:rfseg.shape[0]-6, 6:rfseg.shape[1]-6, :]
-
-segimg = np.argmax(rfseg, 2)
-segimglab, segncc = label( segimg==0, np.ones((3,3)) )
-segseed = (segimg==2)*1
-segseed = segseed*(sp.ndimage.binary_fill_holes(segimg==0)>0)
-
-#for ii in np.arange(1, segncc,1):
-#    if( (np.sum((1*(segimglab == ii) )*segseed) == 0) ):
-#        segimglab[segimglab==ii] = 0
-#        
-
-segimglab = cb.removeBoundaryLabel(segimglab)
-#segseed = segseed*segimglab>0
+# training set performance
+rftrain = []
+ntrain = []
+# 
+for ii in range(np.shape(rftrainfiles)[0]):
+    rfseg = cv2.imread(rftrainfiles[ii], cv2.CV_LOAD_IMAGE_UNCHANGED)
+    rfseg = rfseg[32:rfseg.shape[0]-32, 30:rfseg.shape[1]-30, :]
     
-segmask = (segimglab>1)*1
+    # neural resize
+    neuralseg = cv2.imread(neuraltrainfiles[ii], cv2.CV_LOAD_IMAGE_UNCHANGED)
+    neuralseg = neuralseg[40:neuralseg.shape[0]-40, 40:neuralseg.shape[1]-40, :]
+    
+    rfseglab = labelSegimg(rfseg)
+    neuralseglab = labelSegimg(neuralseg)
+    
+    rowdiff = gttrainlab.shape[0]-rfseg.shape[0]
+    coldiff = gttrainlab.shape[1]-rfseg.shape[1]
+    gttrainlab = gttrainlab[rowdiff/2:(gttrainlab.shape[0]-rowdiff/2), coldiff/2:(gttrainlab.shape[1]-coldiff/2)]
+    #gtlab, nccgt = label(gt,np.ones((3,3)))
+    
+    gttrainlab = cb.labelDilation(gttrainlab,2)
+    
+    #%%
+    recall1, precision1, fmeasure1, dicescore1 = cb.evaluateSegmentation2(gttrainlab, rfseglab)
+    recall2, precision2, fmeasure2, dicescore2 = cb.evaluateSegmentation2(gttrainlab, neuralseglab)
 
-segseed, ncc = label(segseed, np.ones((3,3)))
-#segseed = cb.removeBoundaryLabel(segseed)
+    rftrain.append(fmeasure1.sum())
+    ntrain.append(fmeasure2.sum())
+    
+    #print recall[1],precision[1], fmeasure[1]
+    bincount = 100
+    ftrf = fthreshValue(fmeasure1, bincount)
+    ftneural = fthreshValue(fmeasure2, bincount)
+    xval = np.linspace(0, 1, bincount)
+    
+    
+#    plt.figure()#, plt.plot(xval, ft1, label='outimg','')#, xval, ft2, 'b', xval, ft3, 'r', xval, ft4, 'g'),plt.title('F-score Elf phase'),plt.legend(('line1','line2','line3','line4'),('outimg','filterout','smoothout','microbetracker')),plt.show()
+#    line1, = plt.plot(xval, ftrf, label='randomforest',color = 'k',linewidth = LINEWIDTH)
+#    line2, = plt.plot(xval, ftneural, label='neural',color = 'b',linewidth = LINEWIDTH)
 
-segimglab = cb.watershedSeeded(np.ones(segmask.shape), segmask, segseed)
 
-#segimglab[segimglab==-1]=0
-#segimglab[segimglab==1]=0
+# testset performance
 
-rowdiff = gt.shape[0]-segimg.shape[0]
-coldiff = gt.shape[1]-segimg.shape[1]
-gtlabel = gtlabel[rowdiff/2:(gt.shape[0]-rowdiff/2), coldiff/2:(gt.shape[1]-coldiff/2)]
-#gtlab, nccgt = label(gt,np.ones((3,3)))
+rftest = []
+ntest = []
+ 
+for ii in range(np.shape(rftestfiles)[0]):
+    rfseg = cv2.imread(rftestfiles[ii], cv2.CV_LOAD_IMAGE_UNCHANGED)
+    rfseg = rfseg[32:rfseg.shape[0]-32, 30:rfseg.shape[1]-30, :]
+    
+    # neural resize
+    neuralseg = cv2.imread(neuraltestfiles[ii], cv2.CV_LOAD_IMAGE_UNCHANGED)
+    neuralseg = neuralseg[40:neuralseg.shape[0]-40, 40:neuralseg.shape[1]-40, :]
+    
+    rfseglab = labelSegimg(rfseg)
+    neuralseglab = labelSegimg(neuralseg)
+    
+    rowdiff = gttestlab.shape[0]-rfseg.shape[0]
+    coldiff = gttestlab.shape[1]-rfseg.shape[1]
+    gttestlab = gttestlab[rowdiff/2:(gttestlab.shape[0]-rowdiff/2), coldiff/2:(gttestlab.shape[1]-coldiff/2)]
+    #gtlab, nccgt = label(gt,np.ones((3,3)))
+    
+    gttrainlab = cb.labelDilation(gttrainlab, 2)
+    
+    #%%
+    recall1, precision1, fmeasure1, dicescore1 = cb.evaluateSegmentation2(gttestlab, rfseglab)
+    recall2, precision2, fmeasure2, dicescore2 = cb.evaluateSegmentation2(gttestlab, neuralseglab)
+    
+    rftest.append(fmeasure1.sum())
+    ntest.append(fmeasure2.sum())    
+    
+    #print recall[1],precision[1], fmeasure[1]
+    bincount = 100
+    ftrf = fthreshValue(fmeasure1, bincount)
+    ftneural = fthreshValue(fmeasure2, bincount)
+    xval = np.linspace(0, 1, bincount)
+    
+    
+#    plt.figure()#, plt.plot(xval, ft1, label='outimg','')#, xval, ft2, 'b', xval, ft3, 'r', xval, ft4, 'g'),plt.title('F-score Elf phase'),plt.legend(('line1','line2','line3','line4'),('outimg','filterout','smoothout','microbetracker')),plt.show()
+#    line1, = plt.plot(xval, ftrf, label='randomforest',color = 'k',linewidth = LINEWIDTH)
+#    line2, = plt.plot(xval, ftneural, label='neural',color = 'b',linewidth = LINEWIDTH)
 
-gtlabel = cb.labelDilation(gtlabel,7)
+
+rftrain = np.array(rftrain)
+ntrain = np.array(ntrain)
+
+rftest = np.array(rftest)
+ntest = np.array(ntest)
+
+netnames = []
+for ii in neuraltrainfiles:    
+    netnames.append(string.split(string.split(ii,'/')[-1], '_iter')[0])
 
 #%%
-recall, precision, fmeasure, dicescore = cb.evaluateSegmentation2(gtlabel, segimglab)
-#print recall[1],precision[1], fmeasure[1]
-bincount = 100
-ft1 = fthreshValue(fmeasure, bincount)
-xval = np.linspace(0, 1, bincount)
+plt.figure()
+line1, = plt.plot(range(rftrain.shape[0]), rftrain, label='rf', color='k')
+line2, = plt.plot(range(ntrain.shape[0]), ntrain, label='neural', color = 'b')
+plt.xlabel('network model ', fontsize=20),plt.ylabel('Area under cumulative fscore', fontsize=20)
+plt.legend(handler_map={line1: HandlerLine2D(numpoints=40)},loc='lower left')
+#plt.show()
+frame=plt.gca()
+#plt.yticks(np.arange(0, 1.25, .25),np.arange(0, 125, 25))
+plt.xticks(range(len(netnames)),netnames)
+plt.tick_params(axis='both', which='major', labelsize=15)
 
-
-plt.figure()#, plt.plot(xval, ft1, label='outimg','')#, xval, ft2, 'b', xval, ft3, 'r', xval, ft4, 'g'),plt.title('F-score Elf phase'),plt.legend(('line1','line2','line3','line4'),('outimg','filterout','smoothout','microbetracker')),plt.show()
-line1, = plt.plot(xval, ft1, label='CBA',color = 'k',linewidth = LINEWIDTH)
-
-
+#%%
+plt.figure()
+line1, = plt.plot(range(rftest.shape[0]), rftest, label='rf', color='k')
+line2, = plt.plot(range(ntest.shape[0]), ntest,label='neural', color = 'b')
+plt.xlabel('network model ', fontsize=20),plt.ylabel('Area under cumulative fscore', fontsize=20)
+plt.legend(handler_map={line1: HandlerLine2D(numpoints=40)},loc='lower left')
+#plt.show()
+frame=plt.gca()
+#plt.yticks(np.arange(0, 1.25, .25),np.arange(0, 125, 25))
+plt.xticks(range(len(netnames)),netnames)
+plt.tick_params(axis='both', which='major', labelsize=15)
 
 
 
 
 #%%
-a1 = cb.findAreaDistribution(gtlabel)
-a2 = cb.findAreaDistribution(segimglab)
-#a3 = cb.findAreaDistribution(cbafilt)
-#a4 = cb.findAreaDistribution(cbasmooth)
-##a5 = cb.findAreaDistribution(microb)
 
+#ma,mi,sa,mva=cp.findCriticalPoints(rfseg[:,:,2],10.)
+#
+#simg = np.zeros_like(rfseg[:,:,2])
+#for ii in ma:
+#    simg[ii[0],ii[1]] = 1
+#cb.myshow(simg)
 
-bincount=50
-ah1, bina1 = np.histogram(a1,bincount)
-ah2, bina2 = np.histogram(a2,bincount)
-
-LINEWIDTH = 2
-plt.figure()#, plt.plot(xval, ft1, label='outimg','')#, xval, ft2, 'b', xval, ft3, 'r', xval, ft4, 'g'),plt.title('F-score Elf phase'),plt.legend(('line1','line2','line3','line4'),('outimg','filterout','smoothout','microbetracker')),plt.show()
-line1, = plt.plot( bina1[:-1], np.float32(ah1)/ah1.sum(), label='ground truth',color = 'c',linewidth = LINEWIDTH)
-line2, = plt.plot(bina2[:-1],np.float32(ah2)/ah2.sum(), label='CBA',color = 'k',linewidth = LINEWIDTH)
+#a1 = cb.findAreaDistribution(gttrainlab)
+#a2 = cb.findAreaDistribution(rfseglab)
+#a3 = cb.findAreaDistribution(neuralseglab)
+##a4 = cb.findAreaDistribution(cbasmooth)
+###a5 = cb.findAreaDistribution(microb)
+#
+##
+#bincount=50
+#ah1, bina1 = np.histogram(a1,bincount)
+#ah2, bina2 = np.histogram(a2,bincount)
+#ah3, bina3 = np.histogram(a3,bincount)
+#
+##
+#LINEWIDTH = 2
+#plt.figure()#, plt.plot(xval, ft1, label='outimg','')#, xval, ft2, 'b', xval, ft3, 'r', xval, ft4, 'g'),plt.title('F-score Elf phase'),plt.legend(('line1','line2','line3','line4'),('outimg','filterout','smoothout','microbetracker')),plt.show()
+#line1, = plt.plot( bina1[:-1], np.float32(ah1)/ah1.sum(), label='ground truth',color = 'c',linewidth = LINEWIDTH)
+#line2, = plt.plot(bina2[:-1],np.float32(ah2)/ah2.sum(), label='rf',color = 'k',linewidth = LINEWIDTH)
+#line3, = plt.plot(bina3[:-1],np.float32(ah3)/ah3.sum(), label='neural',color = 'b',linewidth = LINEWIDTH)
